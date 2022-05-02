8f4e11b5fd4e20ad3aa493ec15e2c7d0c0e107f8
cuilei <allen5205189@gmail.com>
2022-05-02 17:33:44 +0800

    add flash

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,7 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
 
-  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  after_update_commit -> { broadcast_replace_later_to "quotes" }
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
